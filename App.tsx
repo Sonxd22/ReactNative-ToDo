ce8248540f145581439aacf4a3531b0cff5e3d5a
Sonxd22 <legohandz@gmail.com>
2025-08-15 16:41:42 +0700

    vd28

--- App.tsx
+++ App.tsx
@@ -4,13 +4,23 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>test</Text>
+      <Text style={styles.text} >Open up App.tsx to start</Text>
+      <Text style={styles.text1} >working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text1:{
+    fontSize : 30,
+    color : "green",
+  },
+  text:{
+      fontSize : 30,
+      color : "red"
+    },
   container: {
     flex: 1,
     backgroundColor: '#fff',
