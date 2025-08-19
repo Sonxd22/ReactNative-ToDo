564d7a490aaa0e7f4f1d8db360e2e6fd8eb47bc4
Sonxd22 <legohandz@gmail.com>
2025-08-19 09:10:48 +0700

    vd29

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+    const [age, setAge] = useState<Number>(22)
+    const [name, setName] = useState<String>('son')
+    const test = false
+    const [person, setPerson] = useState([{
+      name : "son",
+      age : 25
+     }]);
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>test</Text>
+      <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>{JSON.stringify(person)}</Text>
       <Text style={styles.text} >Open up App.tsx to start</Text>
       <Text style={styles.text1} >working on your app!</Text>
       <StatusBar style="auto" />
